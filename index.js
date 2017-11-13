3d7b8c8c56c21f06b5217863d86d3dd732b5d47d
Christopher Carr <christopher.andrew.carr@gmail.com>
2017-11-13 11:22:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,15 @@ function ternaryCheckCity(city){
   }
 }
 
-function switchOnCharmFromTip ()
+function switchOnCharmFromTip(tip){
+  if (tip = 'generous'){
+    return 'Thank you so much'
+  } else if (tip = 'not as generous'){
+    return 'Thank you'
+  } else if (tip = 'thanks for everything'){
+    return 'Bye.'
+  } else 
+  
+  }
+}
 
